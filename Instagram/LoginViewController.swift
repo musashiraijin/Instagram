cd4a28d655b1813f50f2c4f07d5b14f2b31073c2
Musashi Raijin <Raijin@raijin.local>
2016-08-15 16:33:36 +0900

    ビルド成功。 アプリ起動時、失敗。 ５章ログイン画面実装まで終わった。

--- Instagram/LoginViewController.swift
+++ Instagram/LoginViewController.swift
@@ -109,7 +109,6 @@ class LoginViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
     }
 
     override func didReceiveMemoryWarning() {

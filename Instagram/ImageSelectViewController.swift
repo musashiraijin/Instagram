1f0fea039e7567f5bd08df7ebb93cc361ce9dd87
Musashi Raijin <Raijin@raijin.local>
2016-08-15 19:22:51 +0900

    ビルド失敗。 ６章まで入力。

--- Instagram/ImageSelectViewController.swift
+++ Instagram/ImageSelectViewController.swift
@@ -8,7 +8,38 @@
 
 import UIKit
 
-class ImageSelectViewController: UIViewController {
+class ImageSelectViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate {
+    
+    @IBAction func handleLibraryButton(sender: AnyObject) {
+        
+        // ライブラリ（カメラロール）を指定してピッカーを開く
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.PhotoLibrary) {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = UIImagePickerControllerSourceType.PhotoLibrary
+            presentViewController(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func handleCameraButton(sender: AnyObject) {
+        
+        // カメラを指定してピッカーを開く
+        if UIImagePickerController.isSourceTypeAvailable(UIImagePickerControllerSourceType.Camera) {
+            let pickerController = UIImagePickerController()
+            pickerController.delegate = self
+            pickerController.sourceType = UIImagePickerControllerSourceType.Camera
+            presentViewController(pickerController, animated: true, completion: nil)
+        }
+    }
+    
+    
+    @IBAction func handleCancelButton(sender: AnyObject) {
+        
+        // 画面を閉じる
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -21,6 +52,30 @@ class ImageSelectViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    func imagePickerController(picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : AnyObject]) {
+        
+        if info[UIImagePickerControllerOriginalImage] != nil {
+            // 撮影/選択された画像を取得する
+            let image = info[UIImagePickerControllerOriginalImage] as! UIImage
+            
+            // ここでpresentViewControllerを呼び出しても表示されないためメソッドが終了してから呼ばれるようにする
+            dispatch_async(dispatch_get_main_queue()) {
+                
+                // AdobeImageEditorを起動する
+                let adobeViewController = AdobeUXImageEditorViewController(image: image)
+                adobeViewController.delegate = self
+                self.presentViewController(adobeViewController, animated: true, completion:  nil)
+            }
+        }
+        
+        // 閉じる
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+    func imagePickerControllerDidCancel(picker: UIImagePickerController) {
+        // 閉じる
+        picker.dismissViewControllerAnimated(true, completion: nil)
+    }
 
     /*
     // MARK: - Navigation

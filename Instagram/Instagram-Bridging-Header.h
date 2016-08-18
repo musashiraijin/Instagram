1d00b82d0e781cdd2d0373667b46aa4e251d04a6
Musashi Raijin <Raijin@raijin.local>
2016-08-18 14:39:39 +0900

    ビルド成功。 タイムライン表示に不具合。

---
+++ Instagram/Instagram-Bridging-Header.h
@@ -0,0 +1,15 @@
+//
+//  Instagram-Bridging-Header.h
+//  Instagram
+//
+//  Created by Musashi Raijin on 2016/08/15.
+//  Copyright © 2016年 masahiro.kitamura. All rights reserved.
+//
+
+#ifndef Instagram_Bridging_Header_h
+#define Instagram_Bridging_Header_h
+
+#import <AdobeCreativeSDKCore/AdobeCreativeSDKCore.h>
+#import <AdobeCreativeSDKImage/AdobeCreativeSDKImage.h>
+
+#endif /* Instagram_Bridging_Header_h */
